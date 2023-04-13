b824f5b7eb3f00f1ec4737c667535b2685c359bb
Bladimir Zink <bladimirzink7@gmail.com>
2023-04-13 20:30:38 -0300

    Creacion del archivo Program
    Contiene los archivos del ejercicio 1 de OpenBootcamp

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿/* EJERCICIO 1
+Console.WriteLine("Hola, Introduce tu nombre");
+string nombre = Console.ReadLine();
+Console.WriteLine("Bienvenido " + nombre);
+*/
+
+/* EJERCICIO 2
+string datetime = DateTime.Now.ToString("hh:mm:ss tt");
+Console.WriteLine("La Hora Actual es: " + datetime);
+*/
