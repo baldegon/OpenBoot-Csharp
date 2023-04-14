41f601d12cb3ca6dd489a82d20305c07c4dd7f05
Bladimir Zink <bladimirzink7@gmail.com>
2023-04-14 17:05:36 -0300

    adicion de ejercicio 2 de openbootcamp

---
+++ Program ejericio2.cs
@@ -0,0 +1,72 @@
+﻿// Ejercicio 1
+/*
+string nombre;
+string apellido;
+int edad;
+string habilidad;
+
+Console.WriteLine("Hola bienvenido, Escriba su Nombre: ");
+nombre = Console.ReadLine();
+Console.WriteLine("Escriba su Apellido: ");
+apellido = Console.ReadLine();
+Console.WriteLine("Ingrese su edad: ");
+edad = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Escriba su habilidad(Ej: Programar): ");
+habilidad = Console.ReadLine();
+
+Console.WriteLine("Bienvenido " + nombre + " " + apellido + " Su edad es: " + edad + " Y tiene la habilidad de: " + habilidad);
+*/
+
+
+// Ejercicio 2
+/*
+int puerta;
+int rueda;
+string marca;
+string vtv;
+
+int peso;
+int largo;
+string material;
+string color;
+*/
+
+
+//EJERCICIO 3
+/*
+int num1 = 1;
+int num2 = 2;
+char abc = 'a';
+
+bool verda = true;
+bool falso = false;
+bool verdad  = true;
+
+if  (num1 > num2)
+{
+    Console.WriteLine("El numero 1 es mayor que el numero 2");
+}
+else
+{
+    Console.WriteLine("El numero 2 es mayor a el numero 1");
+}
+
+if (abc == 'a')
+{
+    Console.WriteLine(abc);
+}
+
+if (verda && falso)
+{
+    Console.WriteLine("Verdad");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+if (verda || verdad)
+{
+    Console.WriteLine("Es verdad");
+}
+*/
